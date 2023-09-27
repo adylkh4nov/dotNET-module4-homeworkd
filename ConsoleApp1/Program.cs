5f994979f87767049996daaf82d905eac69895e5
Olzhas Adylkhanov <adylkh4nov@gmail.com>
2023-09-27 19:47:14 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dotNET_module4_homeworkd;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Plane.CompanyName = "Panam Airlines";
+
+            Plane[] planes = new Plane[5];
+            planes[0] = new Plane("Boeing 747", 400, 560.5);
+            planes[1] = new Plane("Airbus A320", 150, 530.2);
+            planes[2] = new Plane("Boeing 777", 550, 590.0);
+            planes[3] = new Plane("Embraer E190", 100, 490.8);
+            planes[4] = new Plane();
+
+            Console.WriteLine($"Total planes created: {Plane.TotalPlanes}");
+
+            Console.WriteLine($"First plane model: {planes[0].GetModel()}");
+            Console.WriteLine($"Is the first plane flying? {planes[0].IsFlying()}");
+            planes[0].SetIsFlying(true);
+            Console.WriteLine($"Is the first plane flying now? {planes[0].IsFlying()}");
+
+            string newModel = "Boeing 737";
+            int newPassengerCapacity = 180;
+            double newMaxSpeed = 550.9;
+            planes[1].UpdateDetails(ref newModel, ref newPassengerCapacity, ref newMaxSpeed);
+            Console.WriteLine($"Updated details of the second plane: {planes[1].GetModel()}, {planes[1].GetPassengerCapacity()}, {planes[1].GetMaxSpeed()}");
+        }
+    }
+}
---
+++ dotNET-module4-homeworkd/Plan.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNET_module4_homeworkd
+{
+    using System;
+
+    public partial class Plane
+    {
+        private string model;
+        private int passengerCapacity;
+        private double maxSpeed;
+        private bool isFlying;
+        private static int totalPlanes;
+        private static string companyName;
+
+        public Plane()
+        {
+            model = "Unknown";
+            passengerCapacity = 0;
+            maxSpeed = 0.0;
+            isFlying = false;
+            totalPlanes++;
+        }
+
+        public Plane(string model, int passengerCapacity, double maxSpeed)
+        {
+            this.model = model;
+            this.passengerCapacity = passengerCapacity;
+            this.maxSpeed = maxSpeed;
+            isFlying = false;
+            totalPlanes++;
+        }
+
+        public string GetModel()
+        {
+            return model;
+        }
+
+        public int GetPassengerCapacity()
+        {
+            return passengerCapacity;
+        }
+
+        public double GetMaxSpeed()
+        {
+            return maxSpeed;
+        }
+
+        public bool IsFlying()
+        {
+            return isFlying;
+        }
+
+        public void SetIsFlying(bool flying)
+        {
+            isFlying = flying;
+        }
+
+        public void UpdateDetails(ref string newModel, ref int newPassengerCapacity, ref double newMaxSpeed)
+        {
+            model = newModel;
+            passengerCapacity = newPassengerCapacity;
+            maxSpeed = newMaxSpeed;
+        }
+
+        public static int TotalPlanes
+        {
+            get { return totalPlanes; }
+        }
+
+        public static string CompanyName
+        {
+            get { return companyName; }
+            set { companyName = value; }
+        }
+
+        static Plane()
+        {
+            companyName = "Без названия";
+            totalPlanes = 0;
+        }
+
+        partial void AdditionalMethod();
+    }
+
+}
